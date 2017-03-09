2bfe5c3af2fba0781cf31a55cb7724aa9970a2ef
DESKTOP-1G7P0N8\diego <diegogonzalezmora@outlook.es>
2017-03-09 21:31:28 +0100

    Deck example

--- Week 2/UsingDeckClass/UsingDeckClass/Program.cs
+++ Week 2/UsingDeckClass/UsingDeckClass/Program.cs
@@ -17,6 +17,31 @@ namespace UsingDeckClass
         /// <param name="args">command-line args</param>
         static void Main(string[] args)
         {
+            Deck deck = new Deck();
+
+            //Print deck empty information
+            bool IsDeckEmpty = deck.Empty;
+
+            Console.WriteLine("Empty: "+IsDeckEmpty+"\n");
+
+            //deck.Print();
+            //// tell the deck to shuffle
+
+
+            ////deck.Shuffle();
+
+            ////Cut the deck
+            //deck.Cut(26);
+
+            //take top card and print
+            Card card = deck.TakeTopCard();
+            Console.WriteLine(card.Rank + " Of " + card.Suit);
+
+            //Console.WriteLine();
+            //deck.Print();
+
+
+
         }
     }
 }
