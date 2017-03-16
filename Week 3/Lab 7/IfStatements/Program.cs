c45996df2ebdf35a2903a176c0652255d51ec32b
DESKTOP-1G7P0N8\diego <diegogonzalezmora@outlook.es>
2017-03-16 23:42:00 +0100

    Lab 7

---
+++ Week 3/Lab 7/IfStatements/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfStatements
+{
+    /// <summary>
+    ///  Demonstrates if statements
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Demonstrates various forms of if statement
+        /// </summary>
+        /// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+            // PROBLEM 1
+
+            // ask for and get user answer
+            Console.Write("What is your month birthday?");
+            string month = Console.ReadLine();
+
+            Console.Write("\nWhat is your birth day?");
+            int day = int.Parse(Console.ReadLine());
+
+            // print appropriate message
+            Console.Write("\n Your birthday is " + month + " " + day+"\n");
+
+            // PROBLEM 2
+            day = day - 1;
+            Console.Write("\n You’ll receive an email reminder on " + month + " " + day + "\n");
+
+        }
+    }
+}
