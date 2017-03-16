b8803a6fcaa312659cef99d851cc93ba0703982a
DESKTOP-1G7P0N8\diego <diegogonzalezmora@outlook.es>
2017-03-16 23:15:22 +0100

    String operations

--- Week 3/StringOperations/StringOperations/Program.cs
+++ Week 3/StringOperations/StringOperations/Program.cs
@@ -17,6 +17,22 @@ namespace StringOperations
         /// <param name="args">command-line args</param>
         static void Main(string[] args)
         {
+            // read in csv string
+            Console.Write("Enter name and percent (name,percent): ");
+            string csvString = Console.ReadLine();
+
+            // find comma location
+            int commaLocation = csvString.IndexOf(',');
+
+            // extract name and percent
+            string name = csvString.Substring(0, commaLocation);
+            float percent = float.Parse(csvString.Substring(commaLocation + 1));
+
+            // print name and percent
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Percent: " + percent);
+
+            Console.WriteLine();
         }
     }
 }
