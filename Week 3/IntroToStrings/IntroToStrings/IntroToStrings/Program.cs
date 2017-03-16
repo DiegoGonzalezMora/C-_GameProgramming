877b2f14279ca617541ceeedb5b62957940c6661
DESKTOP-1G7P0N8\diego <diegogonzalezmora@outlook.es>
2017-03-16 21:04:28 +0100

    String info

--- Week 3/IntroToStrings/IntroToStrings/IntroToStrings/Program.cs
+++ Week 3/IntroToStrings/IntroToStrings/IntroToStrings/Program.cs
@@ -6,11 +6,33 @@ using System.Threading.Tasks;
 
 namespace IntroToStrings
 {
+    /// <summary>
+    /// Demostrates string basics
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Demostrates string basics
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            assas  
+            //prompt for and read in gamertag
+            Console.Write("Enter gamertag: ");
+            string gamertag = Console.ReadLine();
+
+            //prompt for and read in level
+            Console.Write("Enter level: ");
+            int level = int.Parse(Console.ReadLine());
+
+            // Extact the first character of the gamertag
+            char firstGamerTagCharacter = gamertag[0];
+
+            //print out values
+            Console.WriteLine("\nGamertag: " + gamertag+
+                "\nLevel: "+level+
+                "\nFirst gamertag char: "+firstGamerTagCharacter+
+                "\n");
         }
     }
 }
