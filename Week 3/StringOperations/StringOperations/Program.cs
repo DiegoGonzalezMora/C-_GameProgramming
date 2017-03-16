2b7ed982d22b96508212683b1c31471862991a08
DESKTOP-1G7P0N8\diego <diegogonzalezmora@outlook.es>
2017-03-16 21:11:27 +0100

    String Operations

---
+++ Week 3/StringOperations/StringOperations/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringOperations
+{
+    /// <summary>
+    /// Demonstrates useful string operations
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Demonstrates IndexOf and Substring methods
+        /// </summary>
+        /// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+        }
+    }
+}
