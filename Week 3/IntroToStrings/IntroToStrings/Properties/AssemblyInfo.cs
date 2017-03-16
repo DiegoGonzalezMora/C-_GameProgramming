537bac1effc72015107b24fe350f24fd575be2c8
DESKTOP-1G7P0N8\diego <diegogonzalezmora@outlook.es>
2017-03-16 23:52:49 +0100

    Week 3

--- Week 3/IntroToStrings/IntroToStrings/Properties/AssemblyInfo.cs
+++ Week 3/IntroToStrings/IntroToStrings/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("IntroToStrings")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("31ecb3fd-7679-417f-af4f-e52ccc4a4648")]
+[assembly: Guid("8e6b31de-8d2a-4912-ab35-6fa736e2df5f")]
 
 // Version information for an assembly consists of the following four values:
 //
