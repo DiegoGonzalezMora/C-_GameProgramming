830fe88290bbc0470129de4d3b83496a294a6361
DESKTOP-1G7P0N8\diego <diegogonzalezmora@outlook.es>
2017-03-16 23:10:34 +0100

    String operations

--- Week 3/StringOperations/StringOperations/Program.cs
+++ Week 3/StringOperations/StringOperations/Program.cs
@@ -6,10 +6,33 @@ using System.Threading.Tasks;
 
 namespace StringOperations
 {
+    /// <summary>
+    /// Demonstrates useful string operations
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Demonstrates IndexOf and Substring methods
+        /// </summary>
+        /// <param name="args">command-line args</param>
         static void Main(string[] args)
         {
+            //Read in csv string
+            Console.Write("Enter name and percent (name, percent): ");
+            string csvString = Console.ReadLine();
+
+            //Find comma location
+            int commalocation = csvString.IndexOf(",");
+
+            //Extract name and percent
+            string name = csvString.Substring(0,commalocation);
+            float percent = float.Parse(csvString.Substring(commalocation+1));
+
+            //Print name and percent
+            Console.Write("\nName: " + name +
+                "\nPercent: " + percent+
+                "\n");
+
         }
     }
 }
