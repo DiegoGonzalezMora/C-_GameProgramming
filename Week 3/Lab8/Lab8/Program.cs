68a4f101af4d4aa8c6788b94d73f836c7dffad1c
DESKTOP-1G7P0N8\diego <diegogonzalezmora@outlook.es>
2017-03-17 01:17:52 +0100

    Switch example and lab 8

---
+++ Week 3/Lab8/Lab8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("<pyramid slot number>,<block letter>,<whether or not the block should be lit>");
+            string userInput = Console.ReadLine();
+
+            int PyramidSlotNumber = int.Parse(userInput.Substring( 0, userInput.IndexOf(",") ));
+            string blockNumber = userInput.Substring(userInput.IndexOf(",")+1, 1);
+            bool lit = bool.Parse(userInput.Substring(userInput.IndexOf(blockNumber)+2));
+
+            // Print
+            Console.Write("Pyramid Slot Number: "+ PyramidSlotNumber+
+                "\nBlock letter: "+blockNumber+
+                "\nLit: "+ lit + "\n");
+        }
+    }
+}
---
+++ Week 3/SwitchStatements/SwitchStatements/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchStatements
+{
+    /// <summary>
+    /// Demonstrates the switch statement
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Demonstrates the switch statement
+        /// </summary>
+        /// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+            // ask for and get user answer
+            Console.Write("Pick up the shiny thing? (y, n): ");
+            char answer = Console.ReadLine()[0];
+
+            switch (answer)
+            {
+                case 'Y':
+                case 'y':
+                    Console.WriteLine("You have the shiny thing.");
+                    break;
+                case 'N':
+                case 'n':
+                    Console.WriteLine("You don't have the shiny thing.");
+                    break;
+
+                default:
+                    Console.WriteLine("You are a n00b.");
+                    break;
+
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
