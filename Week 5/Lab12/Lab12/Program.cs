71c82d4f2d9931f0ca4de4c8b945db0f85325c8b
DESKTOP-1G7P0N8\diego <diegogonzalezmora@outlook.es>
2017-04-02 19:27:05 +0200

    Made Lab12 about arrays

---
+++ Week 5/Lab12/Lab12/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleCards;
+
+namespace Lab12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck deck = new Deck();
+            Card[] cards = new Card[4];
+
+            deck.Shuffle();
+
+            cards[0] = deck.TakeTopCard();
+            cards[0].FlipOver();
+            cards[0].Print();
+
+            Console.WriteLine();
+
+            cards[1] = deck.TakeTopCard();
+            cards[1].FlipOver();
+            cards[0].Print();
+            cards[1].Print();
+
+        }
+    }
+}
