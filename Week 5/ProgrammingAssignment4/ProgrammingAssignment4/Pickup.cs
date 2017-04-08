3f4b8f8ee8b5662ed78388cbbef05f42d9127d16
DESKTOP-1G7P0N8\diego <diegogonzalezmora@outlook.es>
2017-04-08 18:09:05 +0200

    Programming Assignment 4

---
+++ Week 5/ProgrammingAssignment4/ProgrammingAssignment4/Pickup.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ProgrammingAssignment4
+{
+    /// <summary>
+    /// A pickup
+    /// </summary>
+    public class Pickup
+    {
+        #region Fields
+
+        // drawing support
+        Texture2D sprite;
+        Rectangle drawRectangle;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="sprite">sprite for the pickup</param>
+        /// <param name="location">location of the center of the pickup</param>
+        public Pickup(Texture2D sprite, Vector2 location)
+        {
+            this.sprite = sprite;
+
+            // STUDENTS: set draw rectangle so pickup is centered on location
+            drawRectangle.Width = sprite.Width;
+            drawRectangle.Height = sprite.Height;
+            drawRectangle.X = (int)location.X;
+            drawRectangle.Y = (int)location.Y;
+
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the collision rectangle for the pickup
+        /// </summary>
+        public Rectangle CollisionRectangle
+        {
+            get { return drawRectangle; }
+        }
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Draws the pickup
+        /// </summary>
+        /// <param name="spriteBatch">sprite batch</param>
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            // STUDENTS: use the sprite batch to draw the pickup
+            spriteBatch.Draw(this.sprite, this.drawRectangle, Color.White);
+        }
+
+        #endregion
+    }
+}
